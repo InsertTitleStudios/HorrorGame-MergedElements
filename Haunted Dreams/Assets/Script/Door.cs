da83f47f68632c207861f33029da029c2a3e053c
Marc Brooks <marc.brooks@students.plymouth.ac.uk>
2016-12-02 06:44:16 +0000

    Added Animations, materials, sky5x and scripts for the hotel

---
+++ Haunted Dreams/Assets/Script/Door.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	private Animator _animator = null;
+
+
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider Collider)
+	{
+		if (Collider.gameObject.tag == "Player") {
+			_animator.SetBool ("isopen", true);
+		}
+	}
+
+	void OnTriggerExit(Collider Collider)
+	{
+		_animator.SetBool ("isopen", false);
+	}
+}
