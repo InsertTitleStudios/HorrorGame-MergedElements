5e3be3239fa7d08286f6f59a06ad9db23cc6a7ad
VS00038\mbrooks7 <marc.brooks@students.plymouth.ac.uk>
2017-01-05 19:15:07 +0000

    cleaning up code. I still need to fix the raycast stuff.

--- Haunted Dreams/Assets/Scripts/RaycastSweep.cs
+++ Haunted Dreams/Assets/Scripts/RaycastSweep.cs
@@ -3,34 +3,19 @@ using System.Collections;
 
 public class RaycastSweep : MonoBehaviour
 {
-
-    public float distance = 10f;
-    public float fov = 45;
+    public float distance = 5f;
+    private float fov = 15;
     public Camera cam;
     public float angle;
     //Changes the spread of raycasts
-    public int segments = 30;
+    private int segments = 30;
     //Segments equals number of lines however incorrect number of segments showing for lines
     public bool enemyHit = false;
-    private Vector3 startPos;
     private Vector3 targetPos;
-    private int startAngle;
-    private int finishAngle;
-    private int increment;
-    public int angleTemp;
-
-    public bool changeDirection = false;
 
     void Start()
-    {
-
-        
+    {     
         targetPos = Vector3.zero;
-
-        //startAngle = (int)(-_angle * 0.5);
-        //finishAngle = (int)(_angle * 0.5);
-
-        //        increment = (int)(_angle / segments);
         StartCoroutine("RaySonar");
     }
 
@@ -42,18 +27,10 @@ public class RaycastSweep : MonoBehaviour
             {
                 RaycastHit hit;
                 Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0, 0.5f));
-
                 float segmentIndex = Mathf.Abs(i);
-                //Debug.Log("i = " + segmentIndex);
-
                 angle = Mathf.Lerp(-fov / 2, fov / 2, segmentIndex / segments);
-
-                Debug.Log(angle);
-
                 yield return null;
-
                 targetPos = (Quaternion.Euler(0, angle, 0) * transform.forward).normalized * distance;
-
                 if (Physics.Raycast(ray.origin, targetPos, out hit))
                 {
                     if (hit.collider.tag == "Small Enemy")
@@ -64,22 +41,14 @@ public class RaycastSweep : MonoBehaviour
                     else if (hit.collider.tag == "Wall")
                     {
                         enemyHit = false;
+                        Debug.Log("Wall");
                     }
                 }
-                Debug.DrawRay(ray.origin, targetPos, Color.red);
+                Debug.DrawRay(ray.origin, targetPos, Color.red, 0.5f);
             }
         }
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
